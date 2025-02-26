0688087702f9ac51c8ad41041852baf6d190f65f
Mark Peters <mapeters101@earthlink.net>
2025-02-26 17:20:21 -0500

    Description for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,17 @@
+'''
+Question 1
+You are given a list of strings from which you want to generate all the unique
+values.
+
+For example, if you were given this list:
+
+['a', 'A', 'b', 'B', 'B', 'A', 'a', 'c']
+your result should contain these values:
+
+['a', 'A', 'b', 'B', 'c']
+Note that the order of the elements in the resulting list is not important.
+
+You can use this list for this exercise.
+
+l = ['AAPL', 'AAPL', 'Aapl', 'aapl', 'MSFT']
+'''
