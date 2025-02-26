7625939ec3c5597df282233fa47448ba53fe49c5
Mark Peters <mapeters101@earthlink.net>
2025-02-26 17:21:25 -0500

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,8 @@
+'''
+Question 2
+Using the same data we saw in Question 1, the goal is to find all the unique
+values in a case-insensitive fashion.
+
+For example, AAPL, Aapl and aapl should each be considered to be the same
+value.
+'''
