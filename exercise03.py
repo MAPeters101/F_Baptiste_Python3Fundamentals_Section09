4e4cafe1d9918793c16f1a00dd06f325ef9be697
Mark Peters <mapeters101@earthlink.net>
2025-02-26 17:22:23 -0500

    Description for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,16 @@
+'''
+Question 3
+Given this data structure:
+
+data = {
+    'd1': {'a': 1, 'b': 2, 'c': 3},
+    'd2': {'b': 20, 'c': 30, 'd': 40},
+    'd3': {'d': 100, 'x': 200}
+}
+Find all the unique keys in the sub-dictionaries.
+
+In this case above, your result should be:
+
+{'a', 'b', 'c', 'd', 'x'}
+Of course, the order in the result is irrelevant (there is no ordering in sets).
+'''
