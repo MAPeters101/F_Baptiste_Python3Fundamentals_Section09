f81c4d09a0dfdfaeceb376a358f963641bb763af
Mark Peters <mapeters101@earthlink.net>
2025-03-09 20:40:13 -0400

    Work on lesson088 to 2:00.

---
+++ lesson088.py
@@ -0,0 +1,44 @@
+s = {'a', 'b', 'c'}
+print(s)
+print(type(s))
+print()
+
+s = set(['a', 'b', 'c'])
+print(s)
+print(type(s))
+print()
+
+s = set('abc')
+print(s)
+print(type(s))
+print()
+
+s = set('python')
+print(s)
+print(type(s))
+print()
+
+s = set(['a', 'a', 'b', 'b'])
+print(s)
+print(type(s))
+print()
+
+s = set('banana')
+print(s)
+print(type(s))
+print()
+
+s = {}
+print(s)
+print(type(s))
+print()
+
+s = set()
+print(s)
+print(type(s))
+print(len(s))
+print('-'*80)
+
+
+
+
