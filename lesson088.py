de03a11005fc1f405b12457da91dd7354d12594b
Mark Peters <mapeters101@earthlink.net>
2025-03-09 20:44:06 -0400

    Complete work on lesson088 from 2:00.

--- lesson088.py
+++ lesson088.py
@@ -40,5 +40,14 @@ print(len(s))
 print('-'*80)
 
 
+s = set('python')
+print('p' in s, 'x' not in s)
+for item in s:
+    print(item)
 
+s2 = s.copy()
+print(s2)
 
+from copy import deepcopy
+s2 = deepcopy(s)
+print(s2)
