7ba4df52e37e41e6ff77f00a58e3ae8f8665092c
Mark Peters <mapeters101@earthlink.net>
2025-03-09 23:20:25 -0400

    Work on lesson090 from to 2:00.

---
+++ lesson090.py
@@ -0,0 +1,18 @@
+s1 = set('abc')
+s2 = {True, False}
+s3 = {'a', 100, 200}
+print(s1.isdisjoint(s2))
+print(s1.isdisjoint(s3))
+print(s3.isdisjoint(s1))
+print()
+
+s = set()
+s.add(100)
+print(s)
+
+s.add(200)
+print(s)
+
+s.add(200)
+print(s)
+
